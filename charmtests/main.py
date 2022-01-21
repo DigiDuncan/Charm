84817b3d6be3c982a05b90181d22892f4589e108
DigiDuncan <digiduncan@gmail.com>
2022-01-21 17:18:36 -0500

    little logos

--- charmtests/main.py
+++ charmtests/main.py
@@ -17,17 +17,33 @@ class MyGame(arcade.Window):
         icon = pyglet_img_from_resource(charmtests.data.images, "charm-icon32t.png")
         self.set_icon(icon)
 
-        self.time = 0
+        self.delta_time = 0.0
+        self.time = 0.0
+
+        arcade.draw_text("abc", 0, 0)  # force font init
+
+        self.fps_label = arcade.pyglet.text.Label("",
+                          font_name='bananaslip plus plus',
+                          font_size=12,
+                          x=0, y=self.height,
+                          anchor_x='left', anchor_y='top',
+                          color = (0, 0, 0) + (0xFF,))
 
         self.title_view = TitleView()
-        arcade.draw_text("abc", 0, 0)  # force font init
 
     def setup(self):
         self.title_view.setup()
         self.show_view(self.title_view)
 
     def update(self, delta_time: float):
+        self.delta_time = delta_time
         self.time += delta_time
+
+    def fps_draw(self):
+        self.fps_label.text = f"{1/self.delta_time:.1f} FPS"
+        with self.ctx.pyglet_rendering():
+            self.fps_label.draw()
+
 
 
 def main():
--- charmtests/views/title.py
+++ charmtests/views/title.py
@@ -17,7 +17,7 @@ class TitleView(arcade.View):
         self.local_time = 0
 
         arcade.set_background_color(CharmColors.FADED_GREEN)
-        self.sprite_list = arcade.SpriteList()
+        self.main_sprites = arcade.SpriteList()
         logo_img = img_from_resource(charmtests.data.images, "logo.png")
         logo_texture = arcade.Texture("logo", logo_img)
         self.logo = arcade.Sprite(texture = logo_texture)
@@ -25,7 +25,7 @@ class TitleView(arcade.View):
         self.logo.center_x = self.size[0] // 2
         self.logo.bottom = self.size[1] // 2
 
-        self.sprite_list.append(self.logo)
+        self.main_sprites.append(self.logo)
 
         self.splash_text = "it has splash text!"
         self.splash_label = arcade.pyglet.text.Label("",
@@ -35,19 +35,51 @@ class TitleView(arcade.View):
                           anchor_x='left', anchor_y='top',
                           color = CharmColors.PURPLE + (0xFF,))
 
+        self.small_logos_forward = arcade.SpriteList()
+        self.small_logos_backward = arcade.SpriteList()
+        small_logo_img = img_from_resource(charmtests.data.images, "small-logo.png")
+        small_logo_texture = arcade.Texture("small_logo", small_logo_img)
+        sprites_horiz = math.ceil(self.size[0] / small_logo_texture.width) + 2
+        sprites_vert = math.ceil(self.size[1] / small_logo_texture.height)
+        self.logo_width = small_logo_texture.width + 20
+        for i in range(sprites_vert):
+            for j in range(sprites_horiz):
+                s = arcade.Sprite(texture = small_logo_texture)
+                s.original_bottom = s.bottom = small_logo_texture.height * i * 1.5
+                s.original_left = s.left = self.logo_width * (j - 1)
+                if i % 2:
+                    self.small_logos_backward.append(s)
+                else:
+                    self.small_logos_forward.append(s)
+
     def on_show(self):
         pass
 
     def on_update(self, delta_time):
         self.local_time += delta_time
+
+        for s in self.small_logos_forward:
+            s.left = s.original_left + ((self.local_time * self.logo_width / 4) % self.logo_width)
+        for s in self.small_logos_backward:
+            s.left = s.original_left - ((self.local_time * self.logo_width / 4) % self.logo_width)
+
+
         m = 0.375
         s = 3
         n = 0.3
         self.logo.scale = bounce(n, m, s, self.window.time)
-        self.splash_label.text = self.splash_text[:max(0, int((self.local_time - 3) * 10))]
+        self.splash_label.text = self.splash_text[:max(0, int((self.local_time - 5) * 10))]
 
     def on_draw(self):
         arcade.start_render()
-        self.sprite_list.draw()
+
+        # Charm BG
+        self.small_logos_forward.draw()
+        self.small_logos_backward.draw()
+
+        # Logo and splash
+        self.main_sprites.draw()
         with self.window.ctx.pyglet_rendering():
             self.splash_label.draw()
+
+        self.window.fps_draw()
