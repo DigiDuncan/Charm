7922b7a926b08e2bd94014e4cc673b6f3882d9e5
DigiDuncan <digiduncan@gmail.com>
2022-01-21 15:51:24 -0500

    icon

--- charmtests/lib/utils.py
+++ charmtests/lib/utils.py
@@ -1,12 +1,18 @@
 import importlib.resources as pkg_resources
 import math
 import PIL.Image
+import arcade
 
 
-def img_from_resource(package: pkg_resources.Package, resource: pkg_resources.Resource):
+def img_from_resource(package: pkg_resources.Package, resource: pkg_resources.Resource) -> PIL.Image.Image:
     with pkg_resources.open_binary(package, resource) as f:
         image = PIL.Image.open(f)
         image.load()
+    return image
+
+def pyglet_img_from_resource(package: pkg_resources.Package, resource: pkg_resources.Resource):
+    with pkg_resources.open_binary(package, resource) as f:
+        image = arcade.pyglet.image.load("icon.png", file = f)
     return image
 
 
--- charmtests/main.py
+++ charmtests/main.py
@@ -1,4 +1,8 @@
 import arcade
+import charmtests
+
+import charmtests.data.images
+from charmtests.lib.utils import pyglet_img_from_resource
 
 from .views.title import TitleView
 
@@ -10,11 +14,20 @@ SCREEN_TITLE = "Charm"
 class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, update_rate=1/120)
+        icon = pyglet_img_from_resource(charmtests.data.images, "charm-icon32t.png")
+        self.set_icon(icon)
+
+        self.time = 0
+
         self.title_view = TitleView()
+        arcade.draw_text("abc", 0, 0)  # force font init
 
     def setup(self):
         self.title_view.setup()
         self.show_view(self.title_view)
+
+    def update(self, delta_time: float):
+        self.time += delta_time
 
 
 def main():
