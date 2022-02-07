82ee021309ac0fc18334a6079301aa4059403b5f
DigiDuncan <digiduncan@gmail.com>
2022-02-07 00:10:15 -0500

    test ballistic

--- charmtests/views/test.py
+++ charmtests/views/test.py
@@ -15,10 +15,10 @@ class TestView(DigiView):
     def setup(self):
         super().setup()
 
-        c = pkg_resources.read_text(charmtests.data.charts.fnf, "test.json")
+        c = pkg_resources.read_text(charmtests.data.charts.fnf, "ballistic.json")
         self.songdata = FNFSong.parse(c)
 
-        with pkg_resources.path(charmtests.data.charts.fnf, "test.ogg") as p:
+        with pkg_resources.path(charmtests.data.charts.fnf, "ballistic.mp3") as p:
             song = arcade.load_sound(p)
             self.song = arcade.play_sound(song, self.volume, looping = False)
         self.window.theme_song.volume = 0
@@ -27,18 +27,19 @@ class TestView(DigiView):
                                         anchor_x="center", anchor_y="center")
         self.player2_text = arcade.Text("????", (self.size[0] // 4), self.size[1] // 2, font_size = 72,
                                         anchor_x="center", anchor_y="center")
+        self.local_time_text = arcade.Text("????", (self.size[0] // 2), 0, font_size = 36,
+                                        anchor_x="center", color=arcade.color.BLACK)
+        self.song_time_text = arcade.Text("????", (self.size[0] // 2), 50, font_size = 18,
+                                        anchor_x="center", color=arcade.color.BLACK)
 
         # Generate "gum wrapper" background
         self.logo_width, self.small_logos_forward, self.small_logos_backward = generate_gum_wrapper(self.size)
-
-    def on_show_view(self):
-        self.song.seek(self.local_time)
-        return super().on_show_view()
 
     def on_key_press(self, symbol: int, modifiers: int):
         match symbol:
             case arcade.key.BACKSPACE:
                 self.back.setup()
+                self.song.delete()
                 self.window.show_view(self.back)
                 arcade.play_sound(self.window.sounds["back"])
 
@@ -49,12 +50,15 @@ class TestView(DigiView):
 
         move_gum_wrapper(self.logo_width, self.small_logos_forward, self.small_logos_backward, delta_time)
 
-        player1notes = [n for n in self.songdata.charts[0].notes if n.position < self.local_time]
+        self.local_time_text._label.text = str(round(self.local_time, 3))
+        self.song_time_text._label.text = str(round(self.song.time, 3))
+
+        player1notes = [n for n in self.songdata.charts[0].notes if n.position <= self.local_time]
         if player1notes:
             player1note = player1notes[-1].lane
             self.player1_text._label.text = wordmap[player1note]
             self.player1_text.color = colormap[player1note]
-        player2notes = [n for n in self.songdata.charts[1].notes if n.position < self.local_time]
+        player2notes = [n for n in self.songdata.charts[1].notes if n.position <= self.local_time]
         if player2notes:
             player2note = player2notes[-1].lane
             self.player2_text._label.text = wordmap[player2note]
@@ -70,5 +74,7 @@ class TestView(DigiView):
 
         self.player1_text.draw()
         self.player2_text.draw()
-
+        self.local_time_text.draw()
+        self.song_time_text.draw()
+        
         super().on_draw()
