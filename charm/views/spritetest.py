308e0d45563469ef41eba3f02e5969f3f2f191ff
DigiDuncan <digiduncan@gmail.com>
2022-02-21 16:09:55 -0500

    better sprite test

--- charm/views/spritetest.py
+++ charm/views/spritetest.py
@@ -1,8 +1,10 @@
+from itertools import cycle
 import arcade
 from charm.lib.adobexml import sprite_from_adobe
 
 from charm.lib.charm import CharmColors, generate_gum_wrapper, move_gum_wrapper
 from charm.lib.digiview import DigiView
+from charm.lib.settings import Settings
 
 
 class SpriteTestView(DigiView):
@@ -12,10 +14,12 @@ class SpriteTestView(DigiView):
     def setup(self):
         super().setup()
 
-        self.sprite = sprite_from_adobe("Whitty")
+        self.sprite = sprite_from_adobe("BOYFRIEND")
         self.sprite.bottom = 0
         self.sprite.left = 0
-        self.sprite.set_animation("Idle")
+        self.sprite.set_animation("boyfriend attack")
+        self.anims = cycle(self.sprite.animations)
+        self.anim_label = arcade.Text("", Settings.width // 2, Settings.height, font_size = 24, color = arcade.color.BLACK, anchor_x="center", anchor_y="top")
 
         # Generate "gum wrapper" background
         self.logo_width, self.small_logos_forward, self.small_logos_backward = generate_gum_wrapper(self.size)
@@ -26,6 +30,10 @@ class SpriteTestView(DigiView):
                 self.back.setup()
                 self.window.show_view(self.back)
                 arcade.play_sound(self.window.sounds["back"])
+            case arcade.key.ENTER:
+                a = next(self.anims)
+                self.sprite.set_animation(a)
+                self.anim_label.value = a
 
         return super().on_key_press(symbol, modifiers)
 
@@ -44,5 +52,6 @@ class SpriteTestView(DigiView):
         self.small_logos_backward.draw()
 
         self.sprite.draw()
+        self.anim_label.draw()
 
         super().on_draw()
