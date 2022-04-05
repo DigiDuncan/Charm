4ee8f9fa1d88c1c78172cd4e0f29386747f26af9
DigiDuncan <digiduncan@gmail.com>
2022-04-05 04:42:35 -0400

    I'm putting too much work into this

--- charm/views/visualizer.py
+++ charm/views/visualizer.py
@@ -1,15 +1,23 @@
 from dataclasses import dataclass
 import importlib.resources as pkg_resources
+import logging
+from random import randint
 
 import arcade
 import librosa
 import nindex
 from numpy import ndarray
+from pyglet.math import Vec2
 
 import charm.data.audio
-from charm.lib.anim import ease_linear
+from charm.lib.anim import ease_linear, ease_quartout
 from charm.lib.digiview import DigiView
+from charm.lib.logsection import LogSection
+from charm.lib.gamemodes.fnf import FNFSong
+from charm.lib.paths import songspath
 from charm.lib.settings import Settings
+
+logger = logging.getLogger("charm")
 
 
 @dataclass
@@ -23,34 +31,92 @@ class Beat:
     time: float
 
 
+colormap = [
+    arcade.color.PURPLE,
+    arcade.color.CYAN,
+    arcade.color.GREEN,
+    arcade.color.MAGENTA
+]
+
+
 class VisualizerView(DigiView):
     def __init__(self, *args, **kwargs):
         super().__init__(fade_in=1, bg_color=arcade.color.BLACK, *args, **kwargs)
 
     def setup(self):
         super().setup()
-        with pkg_resources.path(charm.data.audio, "fourth_wall.wav") as p:
-            self._song = arcade.load_sound(p)
-            load = librosa.load(p, mono=True)
-        self.waveform: ndarray[float] = load[0]
-        self.sample_rate: int = load[1]
 
-        samples: list[SoundPoint] = []
-        for n, s in enumerate(self.waveform):
-            samples.append(SoundPoint((1 / self.sample_rate) * n, s))
-        self.samples = nindex.Index(samples, "time")
+        # Load song and get waveform
+        with LogSection(logger, "loading song and waveform"):
+            with pkg_resources.path(charm.data.audio, "fourth_wall.wav") as p:
+                self._song = arcade.load_sound(p)
+                load = librosa.load(p, mono=True)
+            self.waveform: ndarray[float] = load[0]
+            self.sample_rate: int = load[1]
 
-        self.bpm, beats = librosa.beat.beat_track(self.waveform, self.sample_rate, units="time")
-        self.beats = nindex.Index([Beat(t) for t in beats[::2]], "time")
+        # Create an index of samples
+        with LogSection(logger, "indexing samples"):
+            samples: list[SoundPoint] = []
+            for n, s in enumerate(self.waveform):
+                samples.append(SoundPoint((1 / self.sample_rate) * n, s))
+            self.samples = nindex.Index(samples, "time")
 
-        self.multiplier = 250
-        self.y = Settings.height // 2
-        self.line_width = 1
-        self.x_scale = 2
-        self.beat_time = 0.5
+        # Create an index of beats
+        with LogSection(logger, "indexing beats"):
+            self.bpm, beats = librosa.beat.beat_track(y = self.waveform, sr = self.sample_rate, units = "time")
+            self.beats = nindex.Index([Beat(t) for t in beats[::2]], "time")
 
-        self.pixels: list[tuple[int, int]] = [(0, 0) * Settings.width]
-        self.last_beat = -self.beat_time
+        self.chart_available = False
+        # Create an index of chart notes
+        with LogSection(logger, "parsing chart"):
+            path = songspath / "fnf" / "fourth-wall"
+            self.songdata = FNFSong.parse(path)
+        if self.songdata:
+            with LogSection(logger, "indexing notes"):
+                self.chart_available = True
+                self.player_chart = nindex.Index(self.songdata.charts[0].notes, "time")
+                enemy_chart = self.songdata.get_chart(2, self.songdata.charts[0].difficulty)
+                self.enemy_chart = nindex.Index(enemy_chart.notes, "time")
+
+        # Create background stars
+        with LogSection(logger, "creating stars"):
+            self.star_camera = arcade.Camera()
+            self.stars = arcade.SpriteList()
+            self.scroll_speed = 10  # px/s
+            stars_per_screen = 100
+            star_height = Settings.height + int(self._song.source.duration * self.scroll_speed)
+            star_amount = int(stars_per_screen * (star_height / Settings.height))
+            logger.info(f"Generating {star_amount} stars...")
+            print(star_height)
+            for i in range(star_amount):
+                sprite = arcade.SpriteCircle(5, arcade.color.WHITE + (255,), True)
+                sprite.center_x = randint(0, Settings.width)
+                sprite.center_y = randint(-(star_height - Settings.height), Settings.height)
+                print(sprite.center_y)
+                self.stars.append(sprite)
+
+        with LogSection(logger, "creating text"):
+            self.text = arcade.Text("Fourth Wall by Jacaris", Settings.width / 2, Settings.height * (0.9),
+            font_name = "Determination Sans", font_size = 32, align="center", anchor_x="center", anchor_y="center", width = Settings.width)
+
+        # Gradient
+        self.gradient = arcade.create_rectangle_filled_with_colors(
+            [(-250, Settings.height), (Settings.width + 250, Settings.height), (Settings.width + 250, -250), (-250, -250)],
+            [arcade.color.BLACK, arcade.color.BLACK, arcade.color.DARK_PASTEL_PURPLE, arcade.color.DARK_PASTEL_PURPLE]
+        )
+
+        # Settings
+        with LogSection(logger, "finalizing setup"):
+            self.multiplier = 250
+            self.y = Settings.height // 2
+            self.line_width = 1
+            self.x_scale = 2
+            self.resolution = 4
+            self.beat_time = 0.5
+
+            # RAM
+            self.pixels: list[tuple[int, int]] = [(0, 0) * Settings.width]
+            self.last_beat = -self.beat_time
 
     def on_show(self):
         self.window.theme_song.volume = 0
@@ -64,8 +130,10 @@ class VisualizerView(DigiView):
         time = self.song.time
         sample_index: SoundPoint = self.samples.lt_index(time)
         if sample_index:
-            samples = self.samples.items[sample_index-(Settings.width*self.x_scale-1):sample_index+1:self.x_scale]
-            self.pixels = [(n, (s.amplitude * self.multiplier + self.y)) for n, s in enumerate(samples)]
+            all_samples = self.samples.items[sample_index-(Settings.width*self.x_scale-1):sample_index+1:self.x_scale]
+            if all_samples:
+                samples = all_samples[::self.resolution] + [all_samples[-1]]
+                self.pixels = [(n * self.resolution, (s.amplitude * self.multiplier + self.y)) for n, s in enumerate(samples)]
         last_beat: Beat = self.beats.lt(self.song.time)
         if last_beat:
             self.last_beat = last_beat.time
@@ -77,21 +145,56 @@ class VisualizerView(DigiView):
                 self.window.show_view(self.back)
                 self.song.delete()
                 arcade.play_sound(self.window.sounds["back"])
+            case arcade.key.SPACE:
+                self.song.pause() if self.song.playing else self.song.play()
+            case arcade.key.NUM_0:
+                self.song.seek(0)
 
         return super().on_key_press(symbol, modifiers)
 
     def on_draw(self):
         self.clear()
-        self.camera.use()
         if not self.shown:
             return
-        flash = int(ease_linear(255, 0, self.last_beat, self.last_beat + self.beat_time, self.song.time))
-        green_amount = int(ease_linear(0, 255, self.last_beat, self.last_beat + self.beat_time, self.song.time))
-        arcade.draw_lrtb_rectangle_filled(
-            0, Settings.width, Settings.height, 0,
-            (0, 255, 255, flash)
-        )
-        line_color = (0, green_amount, 0, 255)
+
+        # Camera zoom
+        zoom = ease_quartout(1, 0.9, self.last_beat, self.last_beat + self.beat_time, self.song.time)
+        self.star_camera.scale = zoom
+        self.text.scale = (1 / zoom)
+
+        # Gradient
+        self.gradient.draw()
+
+        # Scroll star camera and draw stars
+        self.star_camera.move_to((0, 0 - (self.song.time * self.scroll_speed)))
+        self.star_camera.use()
+        self.stars.draw()
+
+        self.camera.use()
+
+        # Text
+        self.text.draw()
+
+        # Note flashes
+        if self.chart_available:
+            player_note = self.player_chart.lt(self.song.time)
+            if player_note:
+                player_color = colormap[player_note.lane]
+                player_time = player_note.time
+                player_opacity = ease_linear(127, 0, player_time, player_time + self.beat_time, self.song.time)
+                player_color = player_color + (int(player_opacity),)
+                arcade.draw_xywh_rectangle_filled(Settings.width / 2, 0, Settings.width / 2, Settings.height, player_color)
+            enemy_note = self.enemy_chart.lt(self.song.time)
+            if enemy_note:
+                enemy_color = colormap[enemy_note.lane]
+                enemy_time = enemy_note.time
+                enemy_opacity = ease_linear(127, 0, enemy_time, enemy_time + self.beat_time, self.song.time)
+                enemy_color = enemy_color + (int(enemy_opacity),)
+                arcade.draw_xywh_rectangle_filled(0, 0, Settings.width / 2, Settings.height, enemy_color)
+
+        line_color = (0, 255, 255, 255)
+        line_outline_color = (255, 255, 255, 255)
+        arcade.draw_line_strip(self.pixels, line_outline_color, self.line_width + 2)
         arcade.draw_line_strip(self.pixels, line_color, self.line_width)
 
         super().on_draw()
