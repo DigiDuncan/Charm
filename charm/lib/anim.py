6bb9d918d000ba3db93f50b0b83249db0987bb6e
DigiDuncan <digiduncan@gmail.com>
2022-11-27 16:12:46 -0500

    finally rename these

--- charm/lib/anim.py
+++ charm/lib/anim.py
@@ -3,8 +3,8 @@ import math
 from .utils import clamp
 
 
-def time_to_zero_one_ramp(start: float, end: float, time: float) -> float:
-    """Convert a time to its progress through the range start -> end, from 0 to 1.
+def find_percent(start: float, end: float, time: float) -> float:
+    """Convert a number to its progress through the range start -> end, from 0 to 1.
 
     https://www.desmos.com/calculator/d2qdk3lceh"""
     if end - start == 0:
@@ -29,13 +29,13 @@ def ease_linear(minimum: float, maximum: float, start: float, end: float, x: flo
        * `start: float`: the beginning of the transition, often a time
        * `end: float`: the end of the transition, often a time
        * `x: float`: the current x, often a time"""
-    x = time_to_zero_one_ramp(start, end, x)
+    x = find_percent(start, end, x)
     return lerp(minimum, maximum, x)
 
 
 def ease_quadinout(minimum: float, maximum: float, start: float, end: float, x: float) -> float:
     """https://easings.net/#easeInOutQuad"""
-    x = time_to_zero_one_ramp(start, end, x)
+    x = find_percent(start, end, x)
     if x < 0.5:
         zo = 2 * x * x
     else:
@@ -45,13 +45,13 @@ def ease_quadinout(minimum: float, maximum: float, start: float, end: float, x: 
 
 def ease_quartout(minimum: float, maximum: float, start: float, end: float, x: float) -> float:
     """https://easings.net/#easeOutQuart"""
-    x = time_to_zero_one_ramp(start, end, x)
+    x = find_percent(start, end, x)
     zo = 1 - math.pow(1 - x, 4)
     return lerp(minimum, maximum, zo)
 
 
 def ease_circout(minimum: float, maximum: float, start: float, end: float, x: float) -> float:
     """https://easings.net/#easeOutCirc"""
-    x = time_to_zero_one_ramp(start, end, x)
+    x = find_percent(start, end, x)
     zo = math.sqrt(1 - math.pow(x - 1, 2))
     return lerp(minimum, maximum, zo)
